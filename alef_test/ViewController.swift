1389a8cd8ec9c1b879ad37849fca8ae9e9b02921
folawer21 <folawer21@mail.ru>
2025-02-18 13:49:33 +0300

    Initial Commit

---
+++ alef_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alef_test
+//
+//  Created by Александр  Сухинин on 18.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
